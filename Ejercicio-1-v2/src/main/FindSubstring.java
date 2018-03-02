54c0da93a9c537bee210e5666fcf25ac55878ae8
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-02 17:08:57 -0300

    First commit

---
+++ Ejercicio-1-v2/src/main/FindSubstring.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class FindSubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1;
+		String str2;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese una cadena: ");
+		str1 = sc.nextLine();
+		System.out.println("Ingrese una cadena: ");
+		str2 = sc.nextLine();
+		
+	}
+
+}

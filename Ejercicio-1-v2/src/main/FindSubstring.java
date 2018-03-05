8872bfa82bf808f05433ab3c73c6dd26eb22f361
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-05 15:14:47 -0300

    Ejercicio 1 terminado

--- Ejercicio-1-v2/src/main/FindSubstring.java
+++ Ejercicio-1-v2/src/main/FindSubstring.java
@@ -6,15 +6,34 @@ public class FindSubstring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str1;
-		String str2;
+		String string;
+		String substring;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese una cadena: ");
-		str1 = sc.nextLine();
+		string = sc.nextLine();
 		System.out.println("Ingrese una cadena: ");
-		str2 = sc.nextLine();
+		substring = sc.nextLine();
 		
+		int result = indexOf(string.toLowerCase(), substring.toLowerCase());
+		
+		if (result == substring.toLowerCase().length()) {
+			System.out.println("TRUE");
+		}else {
+			System.out.println("FALSE");
+		}
+	}
+
+	private static final int indexOf(String string, String substring) {
+	    outerloop: for (int i = 0; i < string.length() - substring.length(); i++) {
+	        for (int j = 0; j < substring.length(); j++) {
+	            if (substring.charAt(j) != string.charAt(i + j)) {
+	                continue outerloop;
+	            }
+	        }
+	        return i;
+	    }
+	    return -1;
 	}
 
 }

71f7bce3d64c969a7984ffcf9378715198409ca5
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-07 00:57:11 -0300

    Agrega manejo de argumentos en el Main

--- Ejercicio-2-v1/src/Main.java
+++ Ejercicio-2-v1/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		//Instanciamos un objeto de tipo ReadFile
 		ReadFile file = new ReadFile();
-		
-		file.findTestsums("input.txt");
+		for (int i = 0; i < args.length; i++) {
+			file.findTestsums(args[i]);
+		}
 	}
 
 }

5c562d932aae1bef89d633054eed8bb2f0403476
Javier Heisecke <javierheiseckee@gmail.com>
2018-03-07 11:59:29 -0300

    comentarios/links
    Agregue comentarios, faltan los links

--- Ejercicio-2-v1/src/Main.java
+++ Ejercicio-2-v1/src/Main.java
@@ -1,9 +1,16 @@
-
+/**
+ * 
+ * @author Jazmin Villagra, Javier Heisecke, Matias Fare
+ *
+ *	Fuentes:
+ *		https://docs.oracle.com/javase/7/docs/api/java/text/NumberFormat.html
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		//Instanciamos un objeto de tipo ReadFile
 		ReadFile file = new ReadFile();
+		//uno a uno ingresamos los archivos por linea de comandos
 		for (int i = 0; i < args.length; i++) {
 			file.findTestsums(args[i]);
 		}
--- Ejercicio-2-v1/src/ReadFile.java
+++ Ejercicio-2-v1/src/ReadFile.java
@@ -5,40 +5,44 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class ReadFile {
-	//declaramos un objeto del tipo BufferedReader
+	//declaramos dos objetos del tipo BufferedReader uno para contar lineas y otro para leer
 	private BufferedReader bf;
 	private BufferedReader bfCount;
 	
 	public void findTestsums(String dir){
-		//el string text contiene las lineas leidas del archivo
-		String text = "";
-		/*el entero testsum contendra el valor ascii final del archivo.
-		 * Si no existe, tendra el valor 0
-		*/
+		//el entero testsum contendra el valor ascii de cada elemento del archivo.		
 		int testsum = 0;
-
+		//Number format es una clase, esta clase Esta clase proporciona la interfaz para formatear y analizar números
 		NumberFormat numberFormat =  NumberFormat.getNumberInstance(Locale.US);
 		
 		try {
 			//instanciamos el objeto lector del archivo(escaner)
 			bf = new BufferedReader(new FileReader(dir));
+			//instanciamos el  objeto que nos ayudara a contar las lineas del archivo
 			bfCount = new BufferedReader(new FileReader(dir));
 			int c = 0;
 			int i = 0;
 			//el string bfRead contendra cada linea leida del archivo
 			String bfRead;
-			//mientras no se llegue al final del archivo, se concatenan las lineas
+			//mientras no se llegue al final del archivo nuestro contador incrementara en uno
 			while ((bfRead = bfCount.readLine()) != null){
 				c++;
 			}
+			//declaramos un vector suma que tendra almacenado el testsum por linea
 			int[] sumas = new int[c];
+			//mientras no se llegue al final del archivo con se convierten los caracteres en valor ascii
 			while ((bfRead = bf.readLine()) != null){
 				for (int j = 0; j < bfRead.length(); j++) {
+					//se almacenan los valores ascii en la variable testsum
 		            testsum += (int) bfRead.charAt(j);
 		        }
+				//se almacena testsum en un vector suma
 				sumas[i] = testsum;
+				//imprime la linea junto con su valor ascii
 				System.out.println(bfRead+", "+sumas[i]+"\n");
+				//cambiamos la posicion del vector suma para almacenar el valor ascii de la siguiente linea en la siguiente posicion
 				i++;
+				//formateamos testsum para usar en la siguiente linea
 				testsum = 0;
 			}
 			
@@ -51,6 +55,7 @@ public class ReadFile {
 		}
 	}
 	
+	//En este metodo hallamos el valor ascii final del archivo
 	public int testsumFinal(int[] sumas){
 		int testsumFinal = 0;
 		for (int i = 0; i < sumas.length; i++) {

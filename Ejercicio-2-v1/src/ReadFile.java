acfd285cd67c61fc28d535c2f244274bfd5e79e8
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-07 00:14:07 -0300

    Lee cada linea e imprime el testsum de cada una

--- Ejercicio-2-v1/src/ReadFile.java
+++ Ejercicio-2-v1/src/ReadFile.java
@@ -1,41 +1,62 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class ReadFile {
 	//declaramos un objeto del tipo BufferedReader
 	private BufferedReader bf;
+	private BufferedReader bfCount;
 	
-	public int compare(String dir){
+	public void findTestsums(String dir){
 		//el string text contiene las lineas leidas del archivo
 		String text = "";
 		/*el entero testsum contendra el valor ascii final del archivo.
 		 * Si no existe, tendra el valor 0
 		*/
 		int testsum = 0;
+
+		NumberFormat numberFormat =  NumberFormat.getNumberInstance(Locale.US);
 		
 		try {
 			//instanciamos el objeto lector del archivo(escaner)
 			bf = new BufferedReader(new FileReader(dir));
+			bfCount = new BufferedReader(new FileReader(dir));
+			int c = 0;
+			int i = 0;
 			//el string bfRead contendra cada linea leida del archivo
 			String bfRead;
 			//mientras no se llegue al final del archivo, se concatenan las lineas
+			while ((bfRead = bfCount.readLine()) != null){
+				c++;
+			}
+			int[] sumas = new int[c];
 			while ((bfRead = bf.readLine()) != null){
-				text = text + bfRead;
-			}
-			//si text tiene al menos un caracter, se calcula el testsum
-			if(text.length()>0){
-				for (int j = 0; j < text.length(); j++) {
-		            testsum += (int) text.charAt(j);
+				for (int j = 0; j < bfRead.length(); j++) {
+		            testsum += (int) bfRead.charAt(j);
 		        }
+				sumas[i] = testsum;
+				System.out.println(bfRead+", "+sumas[i]+"\n");
+				i++;
+				testsum = 0;
 			}
+			
+			System.out.println("Archivo: "+dir+". Testsum final: "+numberFormat.format(testsumFinal(sumas)));
 		} catch (Exception e) {
 			//Mensaje de error generico
 			System.out.println("Ha ocurrido un error. No se pudo realizar la comparacion."+"\n");
 			//Excepcion recibida
 			System.err.println(e);
 		}
-		//retorna siempre el valor de testsum
-		return testsum;
+	}
+	
+	public int testsumFinal(int[] sumas){
+		int testsumFinal = 0;
+		for (int i = 0; i < sumas.length; i++) {
+			testsumFinal+=sumas[i];
+		}
+		return testsumFinal;
+		
 	}
 }

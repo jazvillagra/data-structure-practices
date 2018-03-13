93dd8eb6edd46150c5d57b023b597d053a13da9c
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-12 22:03:52 -0300

    Se introduce fecha

--- farmacia_salud/src/lab/edd/Fecha.java
+++ farmacia_salud/src/lab/edd/Fecha.java
@@ -4,4 +4,23 @@ public class Fecha {
 	int aaaa;
 	int mm;
 	int dd;
+	public int getAaaa() {
+		return aaaa;
+	}
+	public void setAaaa(int aaaa) {
+		this.aaaa = aaaa;
+	}
+	public int getMm() {
+		return mm;
+	}
+	public void setMm(int mm) {
+		this.mm = mm;
+	}
+	public int getDd() {
+		return dd;
+	}
+	public void setDd(int dd) {
+		this.dd = dd;
+	}
+	
 }
--- farmacia_salud/src/lab/edd/Main.java
+++ farmacia_salud/src/lab/edd/Main.java
@@ -1,9 +1,19 @@
 package lab.edd;
 
-public class Main extends Lotes {
+import java.util.Scanner;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		Main p=new Main();
+		Scanner sc = new Scanner(System.in);
+		Fecha vencimiento = new Fecha();
+		Lotes lote = new Lotes();
+		System.out.println("Ingrese el año: ");
+		vencimiento.setAaaa(sc.nextInt());
+		System.out.println("Ingrese el mes(1-12): ");
+		vencimiento.setMm(sc.nextInt());
+		System.out.println("Ingrese el año(1-31): ");
+		vencimiento.setDd(sc.nextInt());
 		
 		
 	}

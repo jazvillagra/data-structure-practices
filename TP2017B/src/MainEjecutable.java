868a3320ca32a5459c61171f1729ef639f9e324a
Jazmin Villagra <jvillagra@sodep.com.py>
2018-03-29 11:23:10 -0400

    Se leen argumentos de las propiedades del proyecto y se cargan en la lista

--- TP2017B/src/MainEjecutable.java
+++ TP2017B/src/MainEjecutable.java
@@ -1,17 +1,16 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import com.sun.tracing.dtrace.ArgsAttributes;
 
 public class MainEjecutable {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		ListaEnlazada L = new ListaEnlazada();
-		L.agregarElemento("ABCDE", 20L);
-		L.agregarElemento("FGHIJ", 30L);
-		L.agregarElemento("KLMNO", 2L);
-		L.agregarElemento("KLMNO", 2L);
-		L.agregarElemento("KLMNO", 2L);
-		
-
+		//cargar elementos de la linea de comandos a la lista
+		cargarElementos(L, args);
 		//imprimir lista completa
-		System.out.print("\nContenido de la lista A: {");
+		System.out.print("\nContenido de la lista A: {\n");
 		Iterador it = L.getIterador();
 		while (it.hasNext()) {
 			Contenedor val = it.next();
@@ -29,6 +28,26 @@ public class MainEjecutable {
 		System.out.println("}\n");
 		//TODO: Poner aqui la rutina que lea los valores de listaLibros, listaPrimerasLetras de los argumentos del programa
 		//y luego ejecute el calculo e imprima la lista resultante en pantalla
-			
+		
+	}
+	
+	public static void cargarElementos(ListaEnlazada L, String[] args) throws ParseException{
+		System.out.println("Cargando Elementos");
+		for(int i=0; i < args.length; i++){
+			int index = args[i].indexOf(",");
+			String str = args[i].substring(0, index);
+			L.agregarElemento(str,extraerCantidad(args[i]));
+		}
+	}
+	public static Long extraerCantidad(String args){
+		String cantidad = "";
+		for (int j = 0; j < args.length(); j++) {
+			Character character = args.charAt(j);
+	        if (Character.isDigit(character)) {
+	        	cantidad += character;
+	        }
+	    }
+		Long num = Long.parseLong(cantidad);
+		return num;
 	}
 }

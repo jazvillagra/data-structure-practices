8842daebda70db688d56604859b6ba431c004693
Javier Heisecke <javierheiseckee@gmail.com>
2018-05-15 21:46:37 -0400

    Usaba la colaprioridad, cambio a colasimple

--- TP-Pilas-Colas/src/main/TestColaPrioridad.java
+++ TP-Pilas-Colas/src/main/TestColaPrioridad.java
@@ -10,7 +10,7 @@ public class TestColaPrioridad {
 	//Hice cambios en todas las clases cambiando el tipo de dato de antig a Date, y el resto hice aca
 	//No funciona...
 	public static void main(String[] args) {
-		ColaPrioridad<String> cola= new ColaPrioridad<>();
+		ColaSimple<Integer> cola=new ColaSimple();
 		  try {
 //			  https://www.tutorialspoint.com/java/java_date_time.htm
 //			  DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
@@ -18,16 +18,16 @@ public class TestColaPrioridad {
 //			  Date todayWithZeroTime = formatter.parse(formatter.format(today));
 				LocalDate ld = LocalDate.now();
 				Date antig=new Date(2010-12-10);
-				cola.agregarCliente("Juan", antig, "Defecto", ld, 'A'); 
+				cola.encolar("Juan", antig, "Defecto", ld); 
 				Date antig2=new Date(2010-12-11);
 				LocalDate ld2 = LocalDate.now();
-				cola.agregarCliente("Juan", antig2, "Defecto", ld2, 'A'); 
+				cola.encolar("jose", antig2, "Defecto", ld2); 
 				Date antig3=new Date(2010-12-12);
 				LocalDate ld3 = LocalDate.now();
-				cola.agregarCliente("Juan", antig3, "Defecto", ld3, 'A'); 
+				cola.encolar("Juan", antig3, "Defecto", ld3); 
 	            int n = 3;
 	            for (int i=1; i<=n; i++) {
-	                System.out.println("Valor " + i + ": " + cola.atenderCliente('A'));
+	                System.out.println("Valor " + i + ": " + cola.desencolar());
 	            }    
 		  }catch (Exception e) {
 			  
